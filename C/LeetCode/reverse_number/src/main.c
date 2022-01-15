c0cbea683086f88de6584bcf7598774abb41a8e1
moonraccoon <sl7@flate.io>
2022-01-15 20:52:07 +0100

    added C reverse number problem

---
+++ C/LeetCode/reverse_number/src/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+int reverse_number(int n);
+
+
+int main() {
+    printf("non-reversed: %d\n", 1234);
+    printf("reversed: %d\n", reverse_number(1234));
+
+    return 0;
+}
+
+int reverse_number(int n) {
+    int out = 0;
+    int last_digit = 0;
+    while (n > 0) {
+        last_digit = n % 10;
+        printf("last_digit: %d\n", last_digit);
+        out = (out * 10) + last_digit;
+        n /= 10;
+    }
+    return out;
+}
