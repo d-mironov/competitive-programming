abb21ddc9eb0fecd6599bc7c1466207dd9b8813c
moonraccoon <sl7@flate.io>
2022-03-14 22:30:32 +0100

    fixed  in

--- C/LeetCode/reverse_number/src/main.c
+++ C/LeetCode/reverse_number/src/main.c
@@ -8,8 +8,8 @@ int reverse_number(int n);
 
 
 int main() {
-    printf("non-reversed: %d\n", 1534236469);
-    printf("reversed: %d\n", reverse_number(1534236469));
+    printf("non-reversed: %d\n", 153423646);
+    printf("reversed: %d\n", reverse_number(153423646));
 
     return 0;
 }
@@ -17,6 +17,13 @@ int main() {
 int reverse_number(int x) {
     int64_t out = 0;
     int64_t last_digit = 0;
+
+    if(x == 0)
+    {
+        return 1;
+        exit(1);
+    }
+
     while (x > 0 || x < 0) {
         last_digit = x % 10;
         if ((out*10) >= INT_MAX || (out*10) <= INT_MIN) {
@@ -25,11 +32,7 @@ int reverse_number(int x) {
         out = (out * 10) + last_digit;     
         x /= 10;
     }
+
     
-    if(x == 0)
-    {
-        return 1;
-        exit(1);
-    }
     return out;
 }
