f5008e5f48a7b166f072d68c7f26cacf145d3fa4
Daniel Mironov <danielmironow@pm.me>
2023-04-02 00:10:15 +0200

    added Rust solutions for Leetcode 278,704 and 35

--- LeetCode/278-first-bad-version/Rust/target/debug/deps/Rust-e7e3d0fb45fb6c99.d
+++
@@ -1,5 +0,0 @@
-/home/mironov/Development/Competitive Programming/LeetCode/278-first-bad-version/Rust/target/debug/deps/Rust-e7e3d0fb45fb6c99.rmeta: src/main.rs
-
-/home/mironov/Development/Competitive Programming/LeetCode/278-first-bad-version/Rust/target/debug/deps/Rust-e7e3d0fb45fb6c99.d: src/main.rs
-
-src/main.rs:
--- LeetCode/704-binary-search/Rust/target/debug/Rust.d
+++
@@ -1,1 +0,0 @@
-/home/mironov/Development/Competitive\ Programming/LeetCode/704-binary-search/Rust/target/debug/Rust: /home/mironov/Development/Competitive\ Programming/LeetCode/704-binary-search/Rust/src/main.rs
